af1f67c69754ffdf23b7e694cd7cc935dea2db94
Vishnuteja <vishnutejabojjam79@gmail.com>
2025-11-20 11:25:32 +0530

    Fix: Added HOST and PORT for Render

--- backend/app.py
+++ backend/app.py
@@ -118,4 +118,5 @@ def recommend():
 #  START APP
 # ----------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
+
